95c735460af077b528848e9f1750a240e1d93d08
Jade Manrique <jade.a.manrique@gmail.com>
2018-02-21 15:33:29 -0800

    saved before clone

---
+++ scripts/scripts2.js
@@ -0,0 +1,15 @@
+var name = "Jade";
+var nameLast = "Manrique";
+var num1 = 23;
+var num2 = 1997;
+var egg = false;
+
+var myFirstArray = ["Ugh","Death","Okay","Getting better","Almost","YAY","Super chill"];
+
+var currentDay = myFirstArray[6];
+
+for (var i = 0; i < 7; i++) {
+
+  console.log(myFirstArray[i])
+
+}
