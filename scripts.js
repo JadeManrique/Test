58d97e369a060c398d756aa0f7d55a7812fe06d0
Jade Manrique <osteo@Jades-MacBook-Pro.local>
2018-02-21 09:32:40 -0800

    modified index

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// Single Line Comment //
+/* Multi-Line Comment */
+/*
+==: Comparison
+===: Type Comparison
+>=: bigger or equal to left side
+<=: smaller of equal to left side
+!=: should not be the same as left side
+*/
+
+alert("Brush your teeth twice a day! Don't forget to floss!");
+
+var firstName = "Jade";
+
+var oneNumber = 200;
+
+var secondNumber = 1;
+
+var sum = oneNumber + secondNumber;
+
+var jadeAge = 20;
+
+if (jadeAge < 21) {
+  console.log("Get out of my bar you rotten kids!");
+} else {
+  console.log("Party on Wayne!")
+}
+
+console.log(sum);
+console.log(firstName);
+console.log(true, false);
