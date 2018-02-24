0510892223d54dd5c86db35984ada588984282a0
Jade Manrique <jade.a.manrique@gmail.com>
2018-02-24 13:15:06 -0800

    added new files

--- scripts/scripts2.js
+++ scripts/scripts2.js
@@ -7,7 +7,19 @@ var quotes = [
   "Empty your mind, be formless, shapeless — like water. Now you put water in a cup, it becomes the cup; You put water into a bottle it becomes the bottle; You put it in a teapot it becomes the teapot. Now water can flow or it can crash. Be water, my friend."
 ];
 
-var randomNumber = Math.random() * 6;
+var randomNumber = Math.random() * quotes.length;
 randomNumber = Math.floor(randomNumber);
 
 document.getElementById("Test").innerHTML = quotes[randomNumber];
+
+var num1 = prompt("Type in the first number.");
+var num2 = prompt("Type in the second number.");
+
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
+var sum = num1 + num2;
+
+console.log(sum);
+
+document.getElementById('Test').innerHTML = "<h1 style=text-align:center;padding-top:300px;>" + sum + "</h1>";
